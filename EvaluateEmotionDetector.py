37079d6393f02a6e1618b6aac926badc59215d6d
Avantika Singh <81291895+avantika-2001@users.noreply.github.com>
2024-01-28 13:54:55 +0530

    Add files via upload

---
+++ EvaluateEmotionDetector.py
@@ -0,0 +1,33 @@
+from keras.preprocessing.image import ImageDataGenerator
+from keras.models import model_from_json
+
+emotion_dict = {0: "Angry", 1: "Disgusted", 2: "Fearful", 3: "Happy", 4: "Neutral", 5: "Sad", 6: "Surprised"}
+
+# load json and create model
+json_file = open('model/emotion_model.json', 'r')
+loaded_model_json = json_file.read()
+json_file.close()
+emotion_model = model_from_json(loaded_model_json)
+
+# load weights into new model
+emotion_model.load_weights("model/emotion_model.h5")
+print("Loaded model from disk")
+
+# Initialize image data generator with rescaling
+test_data_gen = ImageDataGenerator(rescale=1./255)
+
+# Preprocess all test images
+test_generator = test_data_gen.flow_from_directory(
+        'data/test',
+        target_size=(48, 48),
+        batch_size=64,
+        color_mode="grayscale",
+        class_mode='categorical')
+
+# do prediction on test data
+predictions = emotion_model.predict(test_generator)
+
+
+
+
+
